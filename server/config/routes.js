1825b6ed881458739450923d1dc020c5c28b90ab
Joshua Huang <jphuangjr@gmail.com>
2016-01-04 21:35:56 -0800

    Implemented google geo for address

--- server/config/routes.js
+++ server/config/routes.js
@@ -8,6 +8,13 @@ var session = require("express-session");
 var accountSid = 'AC929473a2cb1685807ff13435f9f96bf8';
 var authToken = '2462ccc8b59727f001265af824ad1471';
 var client = require('twilio')(accountSid, authToken)
+var geocodeProvider = 'google';
+var httpAdapter = 'https';
+var extra = {
+    apiKey: "",
+    formatter: null
+};
+var geocoder = require('node-geocoder')(geocodeProvider, httpAdapter, extra);
 
 
 
@@ -38,73 +45,99 @@ module.exports = function (app, express) {
             res.redirect('/#/loggedIn');
         });
 
-    app.get("/getPrices", function(req, res){
+    app.post("/getPrices", function(req, res){
+        console.log(req.body);
+        var trip = [];
+        var address1 = req.body.addy1 /*"948 Market Street, San Fransisco"*/;
+        var address2 = req.body.addy2 /*"1455 Market Street, San Fransisco"*/;
+
+
+        geocoder.geocode(address1, function(err, res){
+            console.log("geocode")
+            trip.push({lat: res[0].latitude, long: res[0].longitude});
+            geocoder.geocode(address2, function(err,res){
+                trip.push({lat: res[0].latitude, long: res[0].longitude})
+                console.log(trip)
+                request({
+                    method: "GET",
+                    url: "https://sandbox-api.uber.com/v1/estimates/price",
+                    headers: {
+                        Authorization: "Token " + auth.serverToken
+                        //"Content-Type": "application/json"
+                    },
+                    form: {
+                        //server_token: auth.serverToken,
+                        start_latitude: trip[0].lat,
+                        start_longitude: trip[0].long,
+                        end_latitude: trip[1].lat,
+                        end_longitude: trip[0].long
+                    }
+                }, function(err, res, body){
+
+                    body = JSON.parse(body);
+                    //console.log(">>>>>>>>>>> POST RESPONSE", body.prices[0])
+                    client.messages.create({
+                        to: "+16502834692",
+                        from: "+16506145695",
+                        body: "~ Textber ~   Price Estimate: " + body.prices[0].estimate + " || Distance: " +body.prices[0].distance +"mi || Service type: " + body.prices[0].display_name
+                    }, function(err, message) {
+                        console.log(message.sid);
+                    });
+                })
+            })
+        })
         console.log(">>>>>>>> Server Token",auth.serverToken)
 
-        request({
-            method: "GET",
-            url: "https://sandbox-api.uber.com/v1/estimates/price",
-            headers: {
-                Authorization: "Token " + auth.serverToken
-                //"Content-Type": "application/json"
-            },
-            form: {
-                //server_token: auth.serverToken,
-                start_latitude: 37.471,
-                start_longitude: -122.2433,
-                end_latitude: 37.7856394,
-                end_longitude: -122.4103922
-            }
-        }, function(err, res, body){
 
-            body = JSON.parse(body);
-            //console.log(">>>>>>>>>>> POST RESPONSE", body.prices[0])
-            client.messages.create({
-                to: "+16502834692",
-                from: "+16506145695",
-                body: "~ Textber ~   Price Estimate: " + body.prices[0].estimate + " || Distance: " +body.prices[0].distance +"mi || Service type: " + body.prices[0].display_name
-            }, function(err, message) {
-                console.log(message.sid);
-            });
-        })
     })
 
-    app.get("/reqRide", function(req, res){
+    app.post("/reqRide", function(req, res){
         console.log(">>>>>>>> Access Token",auth.accessToken)
         console.log(">>>>>>>> Access Token",auth.accessToken2)
         console.log(">>>>>>>> Server Token",auth.serverToken)
+        var trip = [];
+        var address1 = req.body.addy1 /*"948 Market Street, San Fransisco"*/;
+        var address2 = req.body.addy2 /*"1455 Market Street, San Fransisco"*/;
+        geocoder.geocode(address1, function(err, res){
+            console.log("geocode")
+            trip.push({lat: res[0].latitude, long: res[0].longitude});
+            geocoder.geocode(address2, function(err,res){
+                trip.push({lat: res[0].latitude, long: res[0].longitude})
+                console.log(trip)
 
-        request({
-            method: "POST",
-            url: "https://sandbox-api.uber.com/v1/requests",
-            headers: [{
-                name: "Content-Type", value : "application/json" },{
-                name: "Authorization", value: "Bearer " + auth.accessToken
-            }],
-            //headers : {
-            //  'Content-Type': "application/json",
-            //    'Authorization': "Bearer " + auth.accessToken
-            //},
-            form: {
-                //server_token: auth.serverToken,
-                product_id: "a1111c8c-c720-46c3-8534-2fcdd730040d",
-                start_latitude: 37.471,
-                start_longitude: -122.2433,
-                end_latitude: 37.7856394,
-                end_longitude: -122.4103922
-            }
-        }, function(err, res, body){
-            console.log("made post req")
+                request({
+                    method: "POST",
+                    url: "https://sandbox-api.uber.com/v1/requests",
+                    headers: [{
+                        name: "Content-Type", value : "application/json" },{
+                        name: "Authorization", value: "Bearer " + auth.accessToken
+                    }],
+                    //headers : {
+                    //  'Content-Type': "application/json",
+                    //    'Authorization': "Bearer " + auth.accessToken
+                    //},
+                    form: {
+                        //server_token: auth.serverToken,
+                        product_id: "a1111c8c-c720-46c3-8534-2fcdd730040d",
+                        start_latitude: 37.471,
+                        start_longitude: -122.2433,
+                        end_latitude: 37.7856394,
+                        end_longitude: -122.4103922
+                    }
+                }, function(err, res, body){
+                    console.log("made post req")
 
-            body = JSON.parse(body);
-            console.log(">>>>>>>>>>> POST RESPONSE", body)
-            client.messages.create({
-                to: "+16502834692",
-                from: "+16506145695",
-                body: "~ Textber ~    " + body
-            }, function(err, message) {
-                console.log(message.sid);
-            });
+                    body = JSON.parse(body);
+                    console.log(">>>>>>>>>>> POST RESPONSE", body.message)
+                    client.messages.create({
+                        to: "+16502834692",
+                        from: "+16506145695",
+                        body: "~ Textber ~    " + body
+                    }, function(err, message) {
+                        console.log(message.sid);
+                    });
+                })
+            })
         })
     })
 
